fa593956c96ee7cba7dc208b3342934916e3651d
Tanya Dubey <dubeytanya2312@gmail.com>
2025-07-15 11:49:06 +0530

    test.js for testing purposes and getting token

---
+++ Logging-Middleware/test.js
@@ -0,0 +1,4 @@
+const { Log } = require('./logger');
+
+Log('backend', 'error', 'handler', 'received string, expected bool');
+Log('backend', 'fatal', 'db', 'Critical database connection failure.');
